4a56e85e20616a7bd7321067402cd25e5df9ad41
Pablo D. Aronno <realcom1@hotmail.com>
2023-09-22 19:55:01 -0300

    Paginaweb

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -30,7 +30,7 @@ router.get('/agregar', (req, res, next) => {
 
 router.post('/agregar', async (req, res, next) => {
   try {
-    if (req.body.titulo != "" && req, body.subtitulo != "" && req.body.cuerpo != "") {
+    if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
       await novedadesmodels.insertNovedades(req.body);
       res.redirect('/admin/novedades')
     } else {
