b4c177af32f7e045a26c128b403520aaa7bb9be6
Pablo D. Aronno <realcom1@hotmail.com>
2023-09-27 20:57:51 -0300

    Paginaweb

--- models/novedadesmodels.js
+++ models/novedadesmodels.js
@@ -25,5 +25,20 @@ async function insertNovedades(obj) {
     }
 }
 
+async function getNovedadById(id) {
+    var query = 'select * from novedades where id = ? ';
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
 
-module.exports = { getNovedades, deleteNovedadesById, insertNovedades };
+async function modificarNovedadById(obj, id) {
+    try {
+        var query = 'update novedades set ? where id=?';
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+     } catch (error) {
+        throw error;
+     }
+}
+
+module.exports = { getNovedades, deleteNovedadesById, insertNovedades, getNovedadById, modificarNovedadById };
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -50,5 +50,42 @@ router.post('/agregar', async (req, res, next) => {
   }
 })
 
+/*diseño del formulario de modificar con novedad cargada*/
+
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  console.log(req.params.id);
+
+  var novedad = await novedadesmodels.getNovedadById(id);
+
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  })
+})
+
+/*actualizacion de modificar*/
+router.post('/modificar', async (req, res, next) => {
+  try {
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+
+
+    await novedadesmodels.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+
+  } catch (error) {
+    console.log(error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modifico la novedad'
+    })
+  }
+})
+
 
 module.exports = router;
